e098d3cf61efb35f169e61458d4a8dfc6861e10f
space1worm <kverenchkhiladzeirakli.work@gmail.com>
2023-11-10 20:09:27 +0400

    updated packages to latest

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,3 +1,1 @@
-export const uuid = () => {
-  return Math.random().toString(36).substring(2) + Date.now().toString(36);
-};
+export const uuid = () => Math.random().toString(36).substring(2) + Date.now().toString(36);
